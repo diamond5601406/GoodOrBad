d85c01295728c2dfb6b78cbe2b7fffce20998764
KeiFunatsuya <jimbaran5601406@gmail.com>
2019-06-13 18:54:11 +0900

    make index.blade.view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/master')
+
+@section('stylesheet')
+<link rel="stylesheet" href="css/index.css">
+@endsection
+
+@section('title', 'ホーム画面')
+
+@section('content')
+<div class="container">
+    <h1>Hello World</h1>
+</div>
+@endsection
