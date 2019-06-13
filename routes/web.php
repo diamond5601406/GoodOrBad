e89c960deb3b86bc955cc936cf67fed902a58634
shoeslover777 <slack.it777@gmail.com>
2019-06-13 18:54:11 +0900

    make index.blade.view

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,6 +12,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index');
