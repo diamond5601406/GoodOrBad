108e7ea6d7a56195e69edaf70bbd602585c65dc4
shoeslover777 <slack.it777@gmail.com>
2019-06-24 11:48:26 +0900

    make validate work correctly

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Input;
 
 class IndexController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
         $lava = new Lavacharts;
 
         $reasons = $lava->DataTable();
@@ -36,13 +37,16 @@ class IndexController extends Controller
         ]);
     }
 
-    public function detail($id) {
+    public function detail($id) 
+    {
         $datas = Post::find($id);
         return view('/detail')->with('datas', $datas);
     }
 
-    public function post(Request $request) {
+    public function post(Request $request)
+     {
         if(Input::get('create')) {
+
             $this->create($request);
         }elseif(Input::get('delete')) {
 
@@ -53,19 +57,37 @@ class IndexController extends Controller
         return $this->index();
     }
 
-      public function create(Request $request) {
+      public function create(Request $request) 
+      {
+        $validateData = $request->validate([
+            'level' => 'required',
+            'title' => 'required|max:255',
+            'content' => 'max:300'
+        ],[
+            'level.required' => 'レベルは必須です。',
+            'title.required' => 'タイトルは必須です。'
+        ]);
         $data = new Post();
         $data->goodOrbad = $request->goodOrbad;
         $data->level = $request->level;
-        $data->title = $request->title;
-        $data->content = $request->content;
+        $data->title = $validateData->title;
+        $data->content = $validateData->content;
         $data->save();
+      }
 
-        // header('Location: /home');
-        // exit;
-    }
+    // public function create(PostRequest $request) {
+    //     $data = new Post();
+    //     $data->goodOrbad = $request->goodOrbad;
+    //     $data->level = $request->level;
+    //     $data->title = $request->title;
+    //     $data->content = $request->content;
+    //     $data->save();
+    //     header('Location: /home');
+    //     exit;
+    //   }
 
-    public function delete(Request $request) {
+    public function delete(Request $request)
+     {
         $habit = Post::find($request->id);
         $habit->delete();
         // return $this->index();
