23e5de280af837aa879bf62ad865bfa406fed62b
shoeslover777 <slack.it777@gmail.com>
2019-06-24 14:41:57 +0900

    made detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,5 +5,35 @@
 @endsection
 
 @section('content')
-    
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                          <td>Good or Bad</td>
+                          <td>{{ $data->goodOrbad }}</td>  
+                        </tr>
+                        <tr>
+                            <td>Level</td>
+                            <td>{{ $data->level }}</td>
+                        </tr>
+                        <tr>
+                            <td>Title</td>
+                            <td>{{ $data->title }}</td>
+                        </tr>
+                        @if(!empty($data->content))
+                        <tr>
+                            <td>Content</td>
+                            <td>{{ $data->content }}</td>
+                        </tr>
+                        @endif
+                        <tr>
+                            <td>Created_at</td>
+                            <td>{{ $data->created_at }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </div>
 @endsection
